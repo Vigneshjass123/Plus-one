b58b391b5025418319738256e2ad020adf3a6ea7
Cyntexia <Cyntexia@DESKTOP-R5AAUJH>
2023-05-12 02:05:48 +0530

    Add project files.

---
+++ Plus One/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plus_One
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //digits = [1,2,3]
+            //Output: [1,2,4]
+            int temp = 1;
+            int[] array = { 1, 2, 3 };
+            for (int i = 0; i < array.Length; i++)
+            {
+               
+                if (array[i] == 3)
+                {
+                   
+                    temp = temp + array[i];
+
+
+
+                    Console.Write(array[0] +""+ i);
+
+                    Console.Write(temp);
+                }
+            }
+        }
+    }
+}
+    
